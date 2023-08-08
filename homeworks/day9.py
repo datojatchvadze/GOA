96177657f5676276765b841d35ec07407e47a921
datojatchvadze <138619309+datojatchvadze@users.noreply.github.com>
2023-08-08 12:55:22 +0400

    day9.py

--- homeworks/day9.py
+++ homeworks/day9.py
@@ -3,12 +3,12 @@ user_age = input("how old are you: ")
 i = 0
 while i < int(user_age):
     print("congratulations you became",i,"years old")
-    i = i + 1
-x = 8
-while x < int(user_age):
-    print("you entered GOA")
-    x = x + 1
-#hw n2
+    i = i + 1 
+    while i == 8:
+        print("congratulations you became",i,"years old and you entered GOA")
+        i = i + 1
+
+# #hw n2
 numbers = 1
 while numbers < 100:
     print(numbers)
