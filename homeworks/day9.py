4d355eb447d9d1eadc960165c42b70a8affad65c
datojatchvadze <138619309+datojatchvadze@users.noreply.github.com>
2023-08-08 12:07:53 +0400

    Add files via upload

---
+++ homeworks/day9.py
@@ -0,0 +1,11 @@
+#hw n1
+user_age = input("how old are you: ")
+for i in range(int(user_age)):
+    print("congratulations,you became",i,"years old")
+for i in range (8,int(user_age)):
+    print("you entered GOA",)
+#hw n2
+numbers = 1
+while numbers < 100:
+    print(numbers)
+    numbers = numbers + 2
